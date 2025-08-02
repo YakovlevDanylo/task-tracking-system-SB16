51c9844f304816be814d358c29dc7eb6227f0f15
YakovlevDanylo <yakovlevdv31@gmail.com>
2025-08-02 17:38:02 +0300

    Add new url and change template name in TaskCreateView

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,5 @@ from tasks import views
 urlpatterns = [
     path("", views.TaskListView.as_view(), name="task-list"),
     path("<int:pk>/", views.TaskDetailView.as_view(), name="task-detail"),
+    path("create-task/", views.TaskCreateView.as_view(), name="task-create"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from tasks.models import Task
-
+from tasks.forms import TaskForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 class TaskListView(ListView):
@@ -9,7 +10,15 @@ class TaskListView(ListView):
     context_object_name = "tasks"
     template_name = "tasks/task_list.html"
 
+
 class TaskDetailView(DetailView):
     model = Task
     context_object_name = "task"
     template_name = "tasks/task_detail.html"
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "tasks/task_form.html"
+    success_url = reverse_lazy("task-list")
