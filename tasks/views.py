e36ee3a8fae9dcccaa813ae9eb5cc521f6ebf95f
YakovlevDanylo <yakovlevdv31@gmail.com>
2025-08-02 17:07:28 +0300

    Create ListView for Tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from tasks.models import Task
+
 
 # Create your views here.
+class TaskListView(ListView):
+    model = Task
+    context_object_name = "tasks"
+    template_name = "tasks/task_list.html"
