72906af5e371265da82c8d60806e1fc1d6865de2
YakovlevDanylo <yakovlevdv31@gmail.com>
2025-08-02 17:22:41 +0300

    Update urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,5 @@ from tasks import views
 
 urlpatterns = [
     path("", views.TaskListView.as_view(), name="task-list"),
+    path("<int:pk>/", views.TaskDetailView.as_view(), name="task-detail"),
 ]
