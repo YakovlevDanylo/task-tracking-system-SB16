29b559846cafea147b567d3d3762d780a77d1c00
YakovlevDanylo <yakovlevdv31@gmail.com>
2025-08-02 16:59:41 +0300

    Create model Task and connect app tasks in settings

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+
+    STATUS_CHOICES = [
+        ("todo", "TO DO"),
+        ("in_progress", "IN PROGRESS"),
+        ("complete", "COMPLETE"),
+    ]
+
+    PRIORITY_CHOICES = [
+        ("low", "LOW PRIORITY"),
+        ("middle", "MIDDLE PRIORITY"),
+        ("high", "HIGH PRIORITY"),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, default="todo")
+    priority = models.CharField(max_length=30, choices=PRIORITY_CHOICES, default="middle")
+    end_time = models.DateField(null=True, blank=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
