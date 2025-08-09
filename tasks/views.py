9535c6002561a63297d87d993877615e180dbc41
YakovlevDanylo <yakovlevdv31@gmail.com>
2025-08-09 16:51:40 +0300

    add login required before adding new task

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from tasks.models import Task
 from tasks.forms import TaskForm
 from django.urls import reverse_lazy
@@ -17,8 +18,12 @@ class TaskDetailView(DetailView):
     template_name = "tasks/task_detail.html"
 
 
-class TaskCreateView(CreateView):
+class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
     form_class = TaskForm
     template_name = "tasks/task_form.html"
     success_url = reverse_lazy("task-list")
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
