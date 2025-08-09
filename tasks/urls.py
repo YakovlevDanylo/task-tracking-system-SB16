7c157b5128083823d96a725fa6ae904d5b182e15
YakovlevDanylo <yakovlevdv31@gmail.com>
2025-08-09 17:14:50 +0300

    Create new templates and update urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,7 @@ from tasks import views
 urlpatterns = [
     path("", views.TaskListView.as_view(), name="task-list"),
     path("<int:pk>/", views.TaskDetailView.as_view(), name="task-detail"),
+    path("<int:pk>/update/", views.TaskUpdateView.as_view(), name="task-update"),
+    path("<int:pk>/delete/", views.TaskDeleteView.as_view(), name="task-delete"),
     path("create-task/", views.TaskCreateView.as_view(), name="task-create"),
 ]
