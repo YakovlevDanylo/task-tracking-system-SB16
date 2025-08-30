38e94fe5c351dbf986312ac92dc0973e11b312e9
YakovlevDanylo <yakovlevdv31@gmail.com>
2025-08-30 16:27:11 +0300

    Login and register custom form

--- auth_system/views.py
+++ auth_system/views.py
@@ -4,18 +4,21 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from auth_system.forms import CustomLoginForm, CustomUserCreationForm
+
 
 # Create your views here.
 class CustomLoginView(LoginView):
     template_name = "auth_system/login.html"
     redirect_authenticated_user = True
+    form_class = CustomLoginForm
 
 class CustomLogoutView(LogoutView):
     next_page = "login"
 
 class RegisterView(CreateView):
     template_name = "auth_system/register.html"
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy("task-list")
 
     def form_valid(self, form):
